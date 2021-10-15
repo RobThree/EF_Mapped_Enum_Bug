81b352cda19ee3a235081ba108123e88c83b3d6b
RobIII <r.janssen@keensystems.eu>
2021-10-15 15:40:50 +0200

    Initial commit

---
+++ EF_Mapped_Enum_Bug/Customer.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace EF_Mapped_Enum_Bug
+{
+    public class Customer
+    {
+        public int Id { get; init; }
+
+        public string Name { get; init; }
+
+        public ApprovalStatus ApprovalStatus { get; init; }
+    }
+
+    public enum ApprovalStatus
+    {
+        New = 'N',
+        Onboarding = 'O',
+        Active = 'A',
+        InActive = 'I'
+    }
+
+    internal class CustomerConfig : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> entity)
+            => entity.Property(v => v.ApprovalStatus)
+                .HasConversion(MyValueConverters.ApprovalStatusConverter)
+                .HasDefaultValue(ApprovalStatus.New);
+    }
+
+    internal class MyValueConverters
+    {
+        public static ValueConverter<ApprovalStatus, char> ApprovalStatusConverter
+            => new ValueConverter<ApprovalStatus, char>(
+                value => (char)value,
+                value => (ApprovalStatus)value
+            );
+    }
+}
---
+++ EF_Mapped_Enum_Bug/TestContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace EF_Mapped_Enum_Bug
+{
+    public class TestContext : DbContext
+    {
+        public virtual DbSet<Customer> Customers => Set<Customer>();
+
+        public TestContext(DbContextOptions<TestContext> dbContextOptions)
+            : base(dbContextOptions)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
